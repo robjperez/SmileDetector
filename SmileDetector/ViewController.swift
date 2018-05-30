1076e48476f7f5e9cd285c29c3b1ab510cfe7558
Roberto Perez Cubero <robj.perez@gmail.com>
2018-05-30 10:38:21 +0200

    Initial Commit

---
+++ SmileDetector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmileDetector
+//
+//  Created by Roberto Perez Cubero on 30/05/2018.
+//  Copyright © 2018 rpc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
